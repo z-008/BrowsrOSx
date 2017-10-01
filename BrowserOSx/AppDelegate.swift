bc5f2ec667dfb34fb1fe75c3e7ee9dda58f53344
Sandeep Samant <sandeepsamant1702@gmail.com>
2017-10-01 12:30:42 +0530

    Initial Commit

---
+++ BrowserOSx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BrowserOSx
+//
+//  Created by Apple on 10/1/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BrowserOSx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BrowserOSx
+//
+//  Created by Apple on 10/1/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
